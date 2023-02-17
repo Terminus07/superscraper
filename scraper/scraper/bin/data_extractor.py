cb188277b33d365e808d1c1d850d14cd2793f432
Terminus07 <p.frangos98@gmail.com>
2023-02-17 03:02:30 +0200

    extract .ts segments of a video

--- scraper/scraper/bin/data_extractor.py
+++ scraper/scraper/bin/data_extractor.py
@@ -5,6 +5,7 @@ import requests
 import validators
 import lxml.etree
 import m3u8
+import os
 
 class StreamInfo():
     bandwidth = None
@@ -37,34 +38,71 @@ def extract_from_selectors(selectors, response:Response) -> None:
 
 def download_videos(video_urls):
     # extract video urls
+    m3u8_content_types = ['application/mpegurl', 'application/x-mpegurl',
+                            'audio/mpegurl', 'audio/x-mpegurl']
+    
+    video_content_types = ['video/mp4', 'video/x-flv', 
+                    'video/3gpp', 'video/ogg', 'video/webm']
+    
     for url in video_urls:
        r = requests.get(url, stream=True)
-       content_type = r.headers.get('Content-Type', None).lower()
-       m3u8_content_types = ['application/mpegurl', 'application/x-mpegurl',
-                             'audio/mpegurl', 'audio/x-mpegurl']
+       content_type = r.headers.get('Content-Type', None)
+       content_length = r.headers.get('content-length', None)
        
+       if content_type:
+           content_type = content_type.lower()
+           print(content_type)
+           print(content_length)
+     
+       # m3u8 response types
        if content_type in m3u8_content_types:
             playlist = get_m3u8_playlist(r,url)
             r = requests.get(playlist.uri)
             segments = m3u8.loads(r.text).data.get('segments')
-            print(segments)
-       else:
-           # regular video link (mp4)
-           print(content_type)
-           with open("file.mp4", "wb") as f:
-            for chunk in r.iter_content(chunk_size=256):
-                f.write(chunk)
-     
+            
+            copy_cmd = 'copy /b '
+            for idx,segment in enumerate(segments):
+                # connect segments together 
+                r = requests.get(segment['uri'])
+                filename = str(idx) +'.ts' 
+                save_file(r, filename, content_length, 1024)
+                
+                if idx == len(segments)-1:
+                    copy_cmd+= filename
+                    copy_cmd+= ' all.ts'
+                else:
+                    copy_cmd+= filename + '+'
+            print(copy_cmd)
+            ffmpeg_cmd = 'ffmpeg -i all.ts -bsf:a aac_adtstoasc -acodec copy -vcodec copy all.mp4'
+            # os.system(copy_cmd)
+        
+       # regular video types         
+       if content_type in video_content_types:
+           save_file(r, "file.mp4", content_length)
+        
+
+def save_file(response, file_name, content_length=None, chunk_size=256):
+    dl = 0
+    content_length = int(content_length)
+    with open(file_name, "wb") as f:
+        for chunk in response.iter_content(chunk_size=chunk_size):
+            if content_length: # content length exists
+                dl += len(chunk)
+                done = int(50 * dl / content_length)
+                print(done, "/", content_length)
+            f.write(chunk)
+
 def get_m3u8_playlist(response:Response,resolution=None):
     playlists = []
     m3u8_object = m3u8.loads(response.text)
-    playlists = m3u8_object.data.get("playlists")
+ 
+    playlists = m3u8_object.data.get("playlists", [])
     playlists = [M3U8Playlist(p) for p in playlists]
     for p in playlists:
         if resolution == p.stream_info.resolution:
             return p
-    
-    return playlists[-1]
+    print(len(playlists))
+    return playlists[0]
     
       
 def wget_download(links):
