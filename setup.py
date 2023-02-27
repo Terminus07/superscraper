ba45c736a936a085731d59ebd9cae6867d14d1c5
Terminus07 <p.frangos98@gmail.com>
2023-02-27 16:11:44 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='superscraper',
+    version='1.0.0',
+    description='CLI web scraping tool and Python library that can be used to extract any data from the web.',
+    long_description=open('README.md').read(),
+    packages=find_packages(),
+    requires=[]
+)
+
